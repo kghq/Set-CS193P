7c3d7667c71d7ffbce64a072f48329672bc7374b
Krzysztof Garmulewicz <garmulewicz@icloud.com>
2024-08-29 10:07:32 +0200

    Initial Commit

---
+++ Set-CS193P/Set_CS193PApp.swift
@@ -0,0 +1,17 @@
+//
+//  Set_CS193PApp.swift
+//  Set-CS193P
+//
+//  Created by Krzysztof Garmulewicz on 29/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct Set_CS193PApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
